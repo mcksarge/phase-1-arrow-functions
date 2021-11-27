7ebd3fa38fbecf519c82bebfff9809fd4c5c41af
mcksarge <higgins.fred.s@gmail.com>
2021-11-27 15:38:45 -0800

    Completed assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide = () => {
+    return 2000 / 100;
+}
+
+const square = (number) => {
+    return number * number;
+}
+
+const add = (num1, num2) => {
+    return num1 + num2;
+}
+
+console.log(add(2, 4));
