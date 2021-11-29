fedf4bb80dd2dca0b670f532e6d92d7501b2cd08
mcksarge <higgins.fred.s@gmail.com>
2021-11-28 21:42:26 -0800

    commit tree b34a3be7130b040b9c03a63d9502be5e8db6a381

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const divide = () => {
+const divide = function () {
     return 2000 / 100;
 }
 
-const square = (number) => {
-    return number * number;
+const square = (int) => {
+    return int * int;
 }
 
 const add = (num1, num2) => {
     return num1 + num2;
 }
 
-console.log(add(2, 4));
+console.log(divide());
